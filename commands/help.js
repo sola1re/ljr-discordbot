f8d65c0d6fa74ac7792c81c218a8c57562099afc
Paul Mandon <64035393+sola1re@users.noreply.github.com>
2020-10-24 18:20:26 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,6 +1,23 @@
 function help(command,msg){
     if(command!==""){
-        msg.reply(`Help command issued. Command was: **${command}**`);//Debug: log content of help command
+        //msg.reply(`Help command issued. Command was: **${command}**`);//Debug: log content of help command
+        
+const Discord = require('discord.js');
+
+const helpEmbed = new Discord.MessageEmbed()
+	.setColor('#ffffff')
+	.setTitle('Commande +help')
+	.setAuthor('St. Marie')
+	.addFields(
+		{ name: 'Toutes les commandes possibles:', value: '+help \n +insta \n +ping' },
+		{ name: '\u200B', value: '\u200B' },
+		{ name: '+insta', value: 'Ex: +insta post \'profile\' 0', inline: true },
+		{ name: '+ping', value: 'donne le ping de l\'utilisateur', inline: true },
+	)
+	.setFooter('Provided by the LJR BOT', 'https://i.imgur.com/YvuTNrM.png');
+
+message.channel.send(helpEmbed);
+
     }
     
     
